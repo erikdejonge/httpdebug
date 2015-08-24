cbdcaddf3c6841d0e4d7224b4dcae240713198ce
Erik de Jonge <erik@a8.nl>
2015-08-24 21:32:21 +0200

    Monday 24 August 2015 (week:34 day:236), 21:32:17

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ created : 19-05-15 / 16:41
 
 from setuptools import setup
 setup(name='httpdebug',
-      version='22',
+      version='23',
       description='Curl based http downloader with debug information',
       url='https://github.com/erikdejonge/httpdebug',
       author='Erik de Jonge',
